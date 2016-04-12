32af622075229dc98a7bda3af3c42464260d92c1
panpanLittlepan <panpan.littlepan@gmail.com>
2016-04-13 01:01:06 +0700

    1.19 edit languageController

--- app/src/app/language/languageControllers.js
+++ app/src/app/language/languageControllers.js
@@ -2,7 +2,7 @@
 'use strict';
   angular
     .module('app')
-    .controller('languageController',languageController);
+    .controller('LanguageController',languageController);
 
 
   /** @ngInject */
